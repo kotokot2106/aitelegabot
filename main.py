2ce39d63e43a7a44c250f0384827e1ecc9132939
kotokot2106 <kotokot2106@gmail.com>
2026-03-02 21:00:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def webhook():
 
     if msg:
         chat_id = msg["chat"]["id"]
-        connection_id = msg.get("business_connection_id")
+        connection_id = msg("business_connection_id")
 
         text = msg.get("text", "User sent non-text message")
 
